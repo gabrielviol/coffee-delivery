fa40e75fef0dfe210124eecc00eeb2ac9c9b07fe
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-08-09 14:53:16 -0300

    Listagem com todos os produtos concluido

--- coffee-delivery/src/CoffeeList/CoffeeCard.tsx
+++ coffee-delivery/src/CoffeeList/CoffeeCard.tsx
@@ -1,29 +1,28 @@
-import { Buy, CoffeeCards, Container, ContainerHeader, List } from './styles'
+import { Buy, CoffeeCards } from './styles'
 import { ShoppingCart, Minus, Plus } from 'phosphor-react'
 
-import coffeeTradicional from '../assets/coffeeTradicional.svg'
 
 interface PropsListCoffee {
     image: string;
     nameCoffee: string;
     type: string;
-    value: number;
     description: string;
 }
 
-export function CoffeeCard({ image, nameCoffee, type, value, description }: PropsListCoffee){
+
+export function CoffeeCard({ image, nameCoffee, type, description }: PropsListCoffee){
     return(
         <CoffeeCards>
             <img src={image} alt="" />
-            <title>Tradicional</title>
+            <title>{type}</title>
             <h2>{nameCoffee}</h2>
-            <p>O tradicional café feito com agua quente e grãos moidos</p>
+            <p>{description}</p>
             <Buy>
                 <h2 className='value'>R$<p className='value2'>9,90</p></h2>
                 <div>
-                    <Minus size={20} weight='fill'/>
+                    <Minus size={20} weight='fill' className='BtMenos'/>
                         <p>1</p>
-                    <Plus size={20} weight='fill'/>
+                    <Plus size={20} weight='fill' className='BtMais'/>
                     <div className='cart'>    
                         <ShoppingCart size={20} weight='fill'/>
                     </div>
--- coffee-delivery/src/CoffeeList/index.tsx
+++ coffee-delivery/src/CoffeeList/index.tsx
@@ -1,49 +1,134 @@
-import { Buy, Container, ContainerHeader, List } from './styles'
-import { ShoppingCart, Minus, Plus } from 'phosphor-react'
+import { Container, ContainerHeader, List } from './styles'
 
 import coffeeTradicional from '../assets/coffeeTradicional.svg'
+import coffeeAmericano from '../assets/coffeeAmericano.svg'
+import coffeeCremoso from '../assets/coffeeCremoso.svg'
+import coffeeGelado from '../assets/coffeeGelado.svg'
+import coffeeLeite from '../assets/coffeeLeite.svg'
+import coffeeLatte from '../assets/coffeeLatte.svg'
+import coffeeCapuccino from '../assets/coffeeCapuccino.svg'
+import coffeeMacchiato from '../assets/coffeeMacchiato.svg'
+import coffeeMocaccino from '../assets/coffeeMocaccino.svg'
+import coffeeChocolate from '../assets/coffeeChocolate.svg'
+import coffeeCubano from '../assets/coffeeCubano.svg'
+import coffeeHavaiano from '../assets/coffeeHavaiano.svg'
+import coffeeArabe from '../assets/coffeeArabe.svg'
+import coffeeIrlandes from '../assets/coffeeIrlandes.svg'
+
 import { CoffeeCard } from './CoffeeCard'
 
 const coffees = [
     {
       key: String,
       image: coffeeTradicional,
-      nameCoffee: 'Espresso Tradicional',
+      nameCoffee: 'Expresso Tradicional',
       type: 'Tradicional',
       value: 9.90,
       description: 'O tradicional café feito com agua quente e grãos moidos'
     },
     {
       key: String,
-      image: coffeeTradicional,
-      nameCoffee: 'Espresso Tradicional 2',
+      image: coffeeAmericano,
+      nameCoffee: 'Expresso Americano',
       type: 'Tradicional',
       value: 9.90,
-      description: 'O tradicional café feito com agua quente e grãos moidos'
+      description: 'Expresso diluído, menos intenso que o tradicional'
     },
     {
       key: String,
-      image: coffeeTradicional,
-      nameCoffee: 'Espresso Tradicional 22',
+      image: coffeeCremoso,
+      nameCoffee: 'Expresso Cremoso',
       type: 'Tradicional',
       value: 9.90,
-      description: 'O tradicional café feito com agua quente e grãos moidos'
+      description: 'Café expresso tradicional com espuma cremosa'
     },
     {
       key: String,
-      image: coffeeTradicional,
-      nameCoffee: 'Espresso Tradicional 2222',
+      image: coffeeGelado,
+      nameCoffee: 'Expresso Gelado',
       type: 'Tradicional',
       value: 9.90,
-      description: 'O tradicional café feito com agua quente e grãos moidos'
+      description: 'Bebida preparada com café expresso e cubos de gelo'
     },
     {
       key: String,
-      image: coffeeTradicional,
-      nameCoffee: 'Espresso Tradicional 233',
+      image: coffeeLeite,
+      nameCoffee: 'Café com Leite',
       type: 'Tradicional',
       value: 9.90,
-      description: 'O tradicional café feito com agua quente e grãos moidos'
+      description: 'Meio a meio de expresso tradicional com leite vaporizado'
+    },
+    {
+      key: String,
+      image: coffeeLatte,
+      nameCoffee: 'Latte',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa'
+    },
+    {
+      key: String,
+      image: coffeeCapuccino,
+      nameCoffee: 'Capuccino',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Bebida com canela feita de doses iguais de café, leite e espuma'
+    },
+    {
+      key: String,
+      image: coffeeMacchiato,
+      nameCoffee: 'Macchiato',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Café expresso misturado com um pouco de leite quente e espuma'
+    },
+    {
+      key: String,
+      image: coffeeMocaccino,
+      nameCoffee: 'Mocaccino',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Café expresso com calda de chocolate, pouco leite e espuma'
+    },
+    {
+      key: String,
+      image: coffeeChocolate,
+      nameCoffee: 'Chocolate Quente',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Bebida feita com chocolate dissolvido no leite quente e café'
+    },
+    {
+      key: String,
+      image: coffeeCubano,
+      nameCoffee: 'Cubano',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Drink gelado de café expresso com rum, creme de leite e hortelã'
+    },
+    {
+      key: String,
+      image: coffeeHavaiano,
+      nameCoffee: 'Havaiano',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Bebida adocicada preparada com café e leite de coco'
+    },
+    {
+      key: String,
+      image: coffeeArabe,
+      nameCoffee: 'Arabe',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Bebida preparada com grãos de café árabe e especiarias'
+    },
+    {
+      key: String,
+      image: coffeeIrlandes,
+      nameCoffee: 'Irlandês',
+      type: 'Tradicional',
+      value: 9.90,
+      description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly'
     }
   ]
 
@@ -63,7 +148,6 @@ export function CoffeeList () {
               image={coffees.image}
               nameCoffee={coffees.nameCoffee}
               type={coffees.type}
-              value={coffees.value}
               description={coffees.description}
 
             />
