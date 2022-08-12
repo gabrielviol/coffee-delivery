3aeec66581af1aed0f2b0ff10c88c040acd246c8
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-08-11 22:12:15 -0300

    Corrigindo bugs na parte do checkout

--- coffee-delivery/src/pages/Home/index.tsx
+++ coffee-delivery/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { CoffeeList } from "./components/CoffeeList";
 import { Intro } from "./components/Intro";
-import { HomeContainer } from "./styles"
+import { HomeContainer } from "./styles";
 
 export function Home(){
   return (
--- coffee-delivery/src/pages/Home/styles.ts
+++ coffee-delivery/src/pages/Home/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
-import background from "../../assets/images/background.svg";
 
 export const HomeContainer = styled.main`
   display: flex;
+  justify-content: center;
   flex-direction: column;
   max-width: 1180px;
   margin: 0 auto;
