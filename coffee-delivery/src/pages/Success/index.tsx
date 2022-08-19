e2963e2d3c5dbc4ad7f4d85f8ce7b324c8fda6b8
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-08-19 20:43:02 -0300

    Estilizando pagina de success

--- coffee-delivery/src/pages/Success/index.tsx
+++ coffee-delivery/src/pages/Success/index.tsx
@@ -1,18 +1,38 @@
-import {  } from './styles'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import { Container, ContainerInfo } from './styles'
 
-export function Success(){
- return(
-    <div>
-        <div>
-            <h1>Uhu! Pedido confirmado</h1>
-            <p>Agora é só aguardar que logo o café chegará até você</p>
-            <div>
-                <p>Entrega em Rua Lorem ipsum dolor sit amet</p>
-                <p>Previsão de entrega</p>
-                <p>Pagamento na entrega</p>
-            </div>
-            <img src="" alt="" />
-        </div>
-    </div>
- )
+import image from '../../assets/image.svg'
+
+export function Success() {
+    return (
+        <Container>
+            <ContainerInfo>
+                <h1>Uhu! Pedido confirmado</h1>
+                <span>Agora é só aguardar que logo o café chegará até você</span>
+                <div className='border'>
+                    <div >
+                        <a className='Map'>
+                            <MapPin size={20} weight='fill' />
+                        </a>
+                        <p>Entrega em Rua Lorem ipsum dolor sit amet</p>
+                    </div>
+
+                    <div >
+                        <a className='Timer'>
+                            <Timer size={20} />
+                        </a>
+                        <p>Previsão de entrega</p>
+                    </div>
+
+                    <div >
+                        <a className='Currency'>
+                            <CurrencyDollar size={20} />
+                        </a>
+                        <p>Pagamento na entrega</p>
+                    </div>
+                </div>
+            </ContainerInfo>
+            <img src={image}/>
+        </Container>
+    )
 }
--- coffee-delivery/src/pages/Success/styles.ts
+++ coffee-delivery/src/pages/Success/styles.ts
@@ -1,2 +1,71 @@
 import styled from "styled-components";
 
+export const Container = styled.main`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8rem;
+    margin-top: 4rem;
+
+    img{
+        display: flex;
+        margin-top: 40px;
+    }
+`
+export const ContainerInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    h1{
+        font-family: 'Baloo 2';
+        font-weight: 800;
+        font-size: 2rem;
+        line-height: 130%;
+        color: ${(props) => props.theme['yellow-dark']};
+    }
+    span{
+        margin-bottom: 30px;
+        font-family: 'Roboto';
+        font-weight: 400;
+        font-size: 1.25rem;
+        line-height: 130%;
+        color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    .border{
+        border: 1px  solid black;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 40px;
+        gap: 32px;
+
+        border-radius: 6px 36px;
+
+        div{
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
+    }
+
+    a{
+        display: flex;
+        align-items: center;
+        padding: 8px;
+        width: 32px;
+        height: 32px;
+        border-radius: 1rem;
+        color: ${(props) => props.theme['white']};
+    }
+    .Map{
+        background-color: ${(props) => props.theme['purple']};      
+    }
+    .Timer{
+        background-color: ${(props) => props.theme['yellow']};
+    }
+    .Currency{
+        background-color: ${(props) => props.theme['yellow-dark']};
+    }
+`
