7f801bc23933e6bb6968f9cb8e10375421bcadfc
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-08-22 20:15:49 -0300

    Refatorando codigo

--- coffee-delivery/src/pages/Checkout/components/Address/index.tsx
+++ coffee-delivery/src/pages/Checkout/components/Address/index.tsx
@@ -1,3 +1,4 @@
+import { Bank, CreditCard, Money } from 'phosphor-react';
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form'
 import { AddressInfo, useCart } from '../../../../hooks/useCart';
@@ -6,7 +7,9 @@ import {
   AddressContainer,
   AddressContent,
   AddressForm,
-  AddressFormGroup
+  AddressFormGroup,
+  PaymentContainer,
+  PaymentOptions
 } from "./styles";
 
 export function Address() {
@@ -14,21 +17,21 @@ export function Address() {
 
   const { addAddress, address } = useContext(useCart);
 
-  function handleCreateAddress(address: any){
+  function handleCreateAddress(address: any) {
     addAddress({
       cep: address.cep,
       rua: address.rua,
-      numero: address.numero, 
-      cidade: address.cidade, 
+      numero: address.numero,
+      cidade: address.cidade,
       complemento: address.complemento,
       pagamento: address.pagamento,
       uf: address.uf
     })
-    console.log(address)
+
   }
 
   console.log(address)
-  
+
   const cep = watch('cep')
   const rua = watch('rua')
   const city = watch('cidade')
@@ -37,62 +40,85 @@ export function Address() {
   const isSubmitDisabled = (!cep || !number || !rua || !city || !uf)
 
   return (
-    <AddressContainer>
-      <h1>Complete seu pedido</h1>
+    <>
+      <AddressContainer>
+        <h1>Complete seu pedido</h1>
 
-      <AddressContent>
-        <div>
-          <p>Endereço de Entrega</p>
-          <span>Informe o endereço onde deseja receber seu pedido</span>
-        </div>
+        <AddressContent>
+          <div>
+            <p>Endereço de Entrega</p>
+            <span>Informe o endereço onde deseja receber seu pedido</span>
+          </div>
 
-        <AddressForm onChange={handleSubmit(handleCreateAddress)}>
-          <AddressFormGroup>
+          <AddressForm onChange={handleSubmit(handleCreateAddress)}>
+            <AddressFormGroup>
+              <input
+                type="text"
+                placeholder="CEP"
+                {...register('cep', { valueAsNumber: true })}
+                required
+              />
+
+            </AddressFormGroup>
             <input
               type="text"
-              placeholder="CEP"
-              {...register('cep', { valueAsNumber: true })}
+              placeholder="Rua"
+              {...register('rua')}
               required
             />
 
-          </AddressFormGroup>
-          <input
-            type="text"
-            placeholder="Rua"
-            {...register('rua')}
-            required
-          />
+            <AddressFormGroup>
+              <input
+                type="text"
+                placeholder="Número"
+                {...register('numero', { valueAsNumber: true })}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Complemento"
+                {...register('complemento')}
+              />
+              <span>Opcional</span>
+            </AddressFormGroup>
+            <AddressFormGroup>
+              <input
+                type="text"
+                placeholder="Cidade"
+                {...register('cidade')}
+                required
+              />
+              <input
+                type="text"
+                placeholder="UF"
+                {...register('uf')}
+                required
+              />
+            </AddressFormGroup>
+          </AddressForm>
+        </AddressContent>
+      </AddressContainer>
+      <PaymentContainer>
+        <p>Pagamento</p>
+        <span>O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
 
-          <AddressFormGroup>
-            <input
-              type="text"
-              placeholder="Número"
-              {...register('numero', { valueAsNumber: true })}
-              required
-            />
-            <input
-              type="text"
-              placeholder="Complemento"
-              {...register('complemento')}
-            />
-            <span>Opcional</span>
-          </AddressFormGroup>
-          <AddressFormGroup>
-            <input
-              type="text"
-              placeholder="Cidade"
-              {...register('cidade')}
-              required
-            />
-            <input
-              type="text"
-              placeholder="UF"
-              {...register('uf')}
-              required
-            />
-          </AddressFormGroup>
-        </AddressForm>
-      </AddressContent>
-    </AddressContainer>
+        <PaymentOptions >
+          <button type="button">
+            <CreditCard />
+            Cartão de Crédito
+          </button>
+
+          <button type="button">
+            <Bank />
+            Cartão de Débito
+          </button>
+
+          <button type="button">
+            <Money />
+            Dinheiro
+          </button>
+        </PaymentOptions>
+      </PaymentContainer>
+    </>
   );
 }
